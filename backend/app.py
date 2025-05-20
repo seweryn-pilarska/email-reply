dc4cd964a8f45a116f9efca8ae7dabc7b9a63967
anna <s22083@pjwstk.edu.pl>
2025-05-20 17:35:32 +0200

    EMR-102 Change to graph solution

--- backend/app.py
+++ backend/app.py
@@ -1,35 +1,24 @@
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
+from agent_graph import build_agent_graph
 import os
-from src.openai_service import OpenAIService
 load_dotenv(dotenv_path="config/.env")
+graph = build_agent_graph()
 
 app = Flask(__name__)
 api_key = os.getenv('OPENAI_API_KEY')
 default_model = os.getenv('DEFAULT_MODEL', 'gpt-4o-mini')
 default_temperature = float(os.getenv('DEFAULT_TEMPERATURE', '0.7'))
 
-openai_service = OpenAIService(
-    api_key=api_key,
-    default_model=default_model,
-    default_temperature=default_temperature
-)
-
 @app.route('/api/chat', methods=['POST'])
 def chat():
     data = request.json
     if not data or 'human_message' not in data:
         return jsonify({'error': 'Human message is required'}), 400
-    response = openai_service.get_response(
-        human_message=data.get('human_message'),
-        system_message=data.get('system_message'),
-        model=data.get('model', default_model),
-        temperature=data.get('temperature', default_temperature)
-    )
-
-    return jsonify({'response': response}) if response else (jsonify({'error': 'OpenAI failed'}), 500)
-
-
+    
+    result = graph.invoke({"email": data.get('human_message')})
+    reply = result.get("reply")
+    return jsonify({'response': reply}) if reply else (jsonify({'error': 'OpenAI failed'}), 500)
 
 @app.route('/api/health', methods=['GET'])
 def health():
